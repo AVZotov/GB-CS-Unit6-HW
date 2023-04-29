55e8b83ff315ed2f5444a7489b6a6932ff3d7e49
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-30 01:12:45 +0300

    Exersise001 project added

---
+++ Exersise001/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] GetIntArray(string userInput)
+{
+    string[] tempArray = userInput.Split(' ');
+    int[] array = new int[tempArray.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(tempArray[i]);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int GetPositiveNumbers(int[] array)
+{
+    int counter = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            counter++;
+    }
+
+    return counter;
+}
+
+Console.Clear();
+Console.Write("Please enter values devided by spacebar: ");
+string userInput = Console.ReadLine();
+int[] array = GetIntArray(userInput);
+PrintArray(array);
+Console.WriteLine($"Amount of values > 0 in array: {GetPositiveNumbers(array)}");
